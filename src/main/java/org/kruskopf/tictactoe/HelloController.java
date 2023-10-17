f52a32648dc86316c35ac6a03e7c1e8d4cf9b471
Mattan41 <Mats.kruskopf@gmail.com>
2023-10-17 11:35:22 +0200

    Initial commit

--- src/main/java/org/kruskopf/tictactoe/HelloController.java
+++ src/main/java/org/kruskopf/tictactoe/HelloController.java
@@ -1,14 +1,26 @@
 package org.kruskopf.tictactoe;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
+    public Button button1;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onLabelClicked(MouseEvent mouseEvent) {
+        welcomeText.setText("");
+        button1.setText("Label was Here!");
+    }
+
+    public void mouseOverLabel(MouseEvent mouseEvent) {
+        button1.setText("Mouse coords: x=" + mouseEvent.getX() + "y=" + mouseEvent.getY());
     }
 }
