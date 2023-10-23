6d2f4e2f63c121dad536f5c912f09ffda2efda37
Mattan41 <Mats.kruskopf@gmail.com>
2023-10-23 17:20:55 +0200

    add checkGameOver

--- src/main/java/org/kruskopf/tictactoe/TicTacToeController.java
+++ src/main/java/org/kruskopf/tictactoe/TicTacToeController.java
@@ -6,10 +6,13 @@ import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ResourceBundle;
 
 public class TicTacToeController {
 
@@ -18,12 +21,12 @@ public class TicTacToeController {
     public Model getModel() {
         return model;
     }
+
     @FXML
     private Button button1;
     @FXML
     private Button button2;
     @FXML
-
     private Button button3;
     @FXML
     private Button button4;
@@ -39,9 +42,11 @@ public class TicTacToeController {
     private Button button9;
 
     List<Button> buttons;
+    @FXML
+    private Text winner;
+    private int playerTurn = 0;
 
-
-    public void initialize() {
+    public void initialize(URL url, ResourceBundle  resourceBundle) {
         buttons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7,button8,button9));
 
         buttons.forEach(this::setButtonProperties);
@@ -54,19 +59,45 @@ public class TicTacToeController {
         button.setFocusTraversable(false);
     }
 
-
     private void setSymbolAndDisable(Button button) {
         setSymbol(button);
         button.setDisable(true);
         checkGameOver();
     }
 
-
     private void setSymbol(Button button) {
-        //Todo: skapa metod för att sätta symbol X eller O i model?
+        if (playerTurn % 2 == 0){
+            button.setText("X");
+            playerTurn = 1;
+        }
+        else{
+            button.setText("O");
+            playerTurn = 0;
+        }
     }
+
     private void checkGameOver() {
-        //Todo: skapa metod i model kolla tre i rad av lika. switch??
+
+        for (int i = 0; i < 8; i++) {
+            String line = switch (i){
+                case 0 -> button1.getText()+button2.getText()+button3.getText();
+                case 1 -> button4.getText()+button5.getText()+button6.getText();
+                case 2 -> button7.getText()+button8.getText()+button9.getText();
+                case 3 -> button1.getText()+button4.getText()+button7.getText();
+                case 4 -> button2.getText()+button5.getText()+button8.getText();
+                case 5 -> button3.getText()+button6.getText()+button9.getText();
+                case 6 -> button1.getText()+button5.getText()+button9.getText();
+                case 7 -> button3.getText()+button5.getText()+button7.getText();
+                default -> null;
+
+            };
+            if(line.equals("XXX"))
+                winner.setText("X vann!");
+
+            else if(line.equals("OOO"))
+                winner.setText("O vann!");
+        }
+
     }
 
     private void restartGame() {
