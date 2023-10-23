bb8237b184ec3d43cd583ed4ed44222eebacd9f8
Mattan41 <Mats.kruskopf@gmail.com>
2023-10-23 15:06:04 +0200

    changed back to buttons.

--- src/main/java/org/kruskopf/tictactoe/Model.java
+++ src/main/java/org/kruskopf/tictactoe/Model.java
@@ -11,47 +11,5 @@ import java.util.List;
 import java.util.Random;
 
 public class Model {
-    public Image image1;
-    public static Image image2;
-    public static Image image3;
-
-    private static ObjectProperty<Image>[][] images;
-
-    public Model() {
-        image1 = new Image(getClass().getResource("images/blank.png").toExternalForm());
-        image2 = new Image(getClass().getResource("images/cross.png").toExternalForm());
-        image3 = new Image(getClass().getResource("images/circle.png").toExternalForm());
-
-        images = new SimpleObjectProperty[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                images[i][j] = new SimpleObjectProperty<>(image1);
-            }
-        }
-    }
-
-    public Image getImage(int row, int col) {
-        return images[row][col].get();
-    }
-
-    public ObjectProperty<Image> imageProperty(int row, int col) {
-        return images[row][col];
-    }
-
-    public static void setImage(int row, int col, Image image) {
-        images[row][col].set(image);
-    }
-
-
-    public static void changeImageToCross(int row, int col) {
-        Image crossImage = image2;
-        setImage(row, col, crossImage);
-    }
-
-    public static void changeImageToCircle(int row, int col) {
-        Image circleImage = image3;
-        setImage(row, col, circleImage);
-    }
-
 
 }
--- src/main/java/org/kruskopf/tictactoe/TicTacToeController.java
+++ src/main/java/org/kruskopf/tictactoe/TicTacToeController.java
@@ -1,10 +1,14 @@
 package org.kruskopf.tictactoe;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import org.kruskopf.tictactoe.Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TicTacToeController {
 
@@ -14,32 +18,68 @@ public class TicTacToeController {
         return model;
     }
     @FXML
-    private ImageView image1;
+    private Button button1;
     @FXML
-    private ImageView image2;
+    private Button button2;
+    @FXML
 
+    private Button button3;
     @FXML
-    private ImageView image3;
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+
+    List<Button> buttons;
 
 
     public void initialize() {
+        buttons = new ArrayList<>(Arrays.asList(button1,button2,button3,button4,button5,button6,button7,button8,button9));
+
+        buttons.forEach(this::setButtonProperties);
+
+    }
+
+
+    public void setButtonProperties(Button button) {
+        button.setOnMouseClicked(event -> setSymbolAndDisable(button));
+        button.setFocusTraversable(false);
     }
 
 
-    public void imageClicked(MouseEvent event) {
-        ImageView imageView = (ImageView) event.getSource();
-        int row = GridPane.getRowIndex(imageView);
-        int col = GridPane.getColumnIndex(imageView);
-
-        // Hämta informationen från getUserData-metoden
-        Object userData = imageView.getUserData();
-
-        // todo: change image to cross or circle dependant on which player/computer
-        if (userData.equals("blank")) {
-            Model.changeImageToCross(row, col);
-        } else if (userData.equals("cross")) {
-            Model.changeImageToCircle(row, col);
-        }
-
+    private void setSymbolAndDisable(Button button) {
+        setSymbol(button);
+        button.setDisable(true);
+        checkGameOver();
     }
+
+
+    private void setSymbol(Button button) {
+        //Todo: skapa metod för att sätta symbol X eller O i model?
+    }
+
+    private void checkGameOver() {
+        //Todo: skapa metod i model kolla tre i rad av lika. switch??
+    }
+    private void restartGame() {
+        //ToDO: skapa metod och knapp för att starta om spelet
+    }
+
+    private void resetButtons() {
+        //todo reset buttons
+    }
+
+    private void pointsTracker() {
+        //TODO: poängräknare
+    }
+    //ToDo: computer controls player 2 in singleplayermode
+    //TODO: SinglePlayer/MultiPlayer choice
+    //TODo: Mutliplayer
 }
