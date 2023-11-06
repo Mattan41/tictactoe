345b56a174297e3883509d91e6676dc6ce0bebab
Mattan41 <Mats.kruskopf@gmail.com>
2023-11-06 12:21:16 +0100

    added choice O or X at start

---
+++ src/main/java/org/kruskopf/tictactoe/Player.java
@@ -0,0 +1,19 @@
+package org.kruskopf.tictactoe;
+
+public class Player {
+
+    private String symbol;
+
+    public Player(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+}
