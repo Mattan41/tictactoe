3e4d569a1b3158be81dfb673a9ee6674ca58aa6c
Mattan41 <Mats.kruskopf.eriksson@iths.se>
2024-04-11 21:44:42 +0200

    test verification on commit

--- src/main/java/org/kruskopf/tictactoe/TicTacToeApplication.java
+++ src/main/java/org/kruskopf/tictactoe/TicTacToeApplication.java
@@ -16,6 +16,7 @@ public class TicTacToeApplication extends Application {
         stage.setTitle("TicTacToe");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
